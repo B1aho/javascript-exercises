2963c85a718b7ade38ffea24655d09cea9fbbe19
B1aho <wheelsurgut00@gmail.com>
2024-08-30 01:12:09 +0500

    Complete 6 task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDividedByFour = isDivided(year, 4);
+    const isDividedByHundread = isDivided(year, 100);
+    const isDividedByFourHundread = isDivided(year, 400);
 
+    if (isDividedByFour && !isDividedByHundread || 
+    isDividedByHundread && isDividedByFourHundread) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
+function isDivided(num, div) {
+    return (num % div) === 0;
+}
 // Do not edit below this line
 module.exports = leapYears;
