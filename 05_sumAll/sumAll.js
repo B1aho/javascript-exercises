b0ddc6823360c0a133e9eb04e269054673a90960
B1aho <wheelsurgut00@gmail.com>
2024-08-30 00:16:27 +0500

    Complete 5 task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if (min < 0 || max < 0) return 'ERROR';
+    if (min > max) {
+        [min, max] = [max, min]
+    }
+    for (let i = min + 1; i <= max; i++) {
+        min += i;
+    }
+    return min;
 };
 
 // Do not edit below this line
