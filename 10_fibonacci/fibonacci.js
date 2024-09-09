1ebdda793fbd76bd32332d3729baf77df274b918
B1aho <wheelsurgut00@gmail.com>
2024-09-09 23:57:13 +0300

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    let a = 1;
+    let b = 1;
+    if (num < 2) return a;
+    for (let i = 0; i < num - 2; i++) {
+        let tmp = b;
+        b += a;
+        a = tmp;
+    }
+    return b;
 };
 
 // Do not edit below this line
