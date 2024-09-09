b5527a5191aeb247bbf1fae97b9dbe46f8dcc476
B1aho <wheelsurgut00@gmail.com>
2024-09-09 23:27:24 +0300

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let arrNormal = [...str].filter(item => alphanumerical.includes(item));
+    let arrReverse = arrNormal.slice().reverse();
+    return (arrNormal.every((value, index) => {return value === arrReverse[index];}));
 };
 
 // Do not edit below this line
