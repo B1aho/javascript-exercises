14641563af36a7c78d1e62f2f431767626ace152
B1aho <wheelsurgut00@gmail.com>
2024-09-10 00:20:57 +0300

    Complete 12th task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const arr = people.toSorted((a, b) => {
+        let currentYear = (new Date()).getFullYear();
+        let first = (a.yearOfDeath) ? (a.yearOfDeath - a.yearOfBirth) : (currentYear - a.yearOfBirth);
+        let second = (b.yearOfDeath) ? (b.yearOfDeath - b.yearOfBirth) : (currentYear - b.yearOfBirth);
+        return second - first;
+    })
+    return arr[0];
 };
 
 // Do not edit below this line
